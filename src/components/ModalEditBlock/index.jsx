3b83d32a090fec7417b16772a95ee08d7f27caec
Artur <artur98games@mail.ru>
2023-08-17 16:48:37 +0700

    changed color in HM, NavBar, add color HTML, changed SVG setgs

--- src/components/ModalEditBlock/index.jsx
+++ src/components/ModalEditBlock/index.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './ModalEditBlock.module.scss';
+import { useDispatch } from 'react-redux';
+import { removeUser } from 'redux/slices/userSlice';
 
 const ModalEditBlock = ({ visibleModalEdit, closeModal }) => {
+  const dispatch = useDispatch();
+
+  const logoutUser = () => {
+    sessionStorage.removeItem('auth');
+    dispatch(removeUser());
+  };
+
   return (
     <div className={visibleModalEdit ? styles.modalVisible : styles.modal}>
       <div className={styles.modalDialog}>
@@ -25,7 +34,7 @@ const ModalEditBlock = ({ visibleModalEdit, closeModal }) => {
             <span>QR-Code</span>
           </Link>
           <Link to="/">
-            <span>Выйти</span>
+            <span onClick={logoutUser}>Выйти</span>
           </Link>
           <Link>
             <span onClick={closeModal}>Отмена</span>
